7476fad5bf4571bbea87d9a0aa573de80b22303d
Furkan Begen <furkanbegen@gmail.com>
2021-10-23 01:57:39 +0300

    Changes color scheme to nord

--- config.h
+++ config.h
@@ -5,16 +5,16 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static int fuzzy = 1;
 static const char *fonts[] = {
-	"Jetbrains Mono:size=18"
+	"Source Code Pro:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#282828" },
-	[SchemeSel] = { "#ebdbb2", "#98971a" },
-	[SchemeSelHighlight] = { "#edbbb2", "#282828" },
-	[SchemeNormHighlight] = { "#edbbb2", "#282828" },
-	[SchemeOut] = { "#ebdbb2", "#98971a" },
+	[SchemeNorm] = { "#8FBCBB", "#2e3340" },
+	[SchemeSel] = { "#D8DEE9", "#B48EAD" },
+	[SchemeSelHighlight] = { "#A3BE8C", "#2e3340" },
+	[SchemeNormHighlight] = { "#A3BE8C", "#2e3340" },
+	[SchemeOut] = { "#ECEFF4", "#D8DEE9" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 20;
